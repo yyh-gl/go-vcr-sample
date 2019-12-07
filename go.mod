348cc29dd5c6f1617b73a55f0f74a2915972ea84
yyh-gl <yhonda.95.gl@gmail.com>
2019-12-07 16:31:00 +0900

    go-vcr導入＆保存処理実装

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/yyh-gl/go-vcr-sample
 
 go 1.12
 
-require github.com/stretchr/testify v1.4.0
+require (
+	cloud.google.com/go v0.49.0 // indirect
+	github.com/dnaeon/go-vcr v1.0.1 // indirect
+	github.com/stretchr/testify v1.4.0
+)
--- main.go
+++ main.go
@@ -4,11 +4,21 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/dnaeon/go-vcr/recorder"
 	"github.com/yyh-gl/go-vcr-sample/qiita"
 )
 
 func main() {
-	qiitaClient := qiita.NewClient(http.DefaultClient)
+	// go-vcr のレコーダを生成
+	// 通信内容は /fixtures/qiita に保存される
+	r, _ := recorder.New("fixtures/qiita")
+	defer r.Stop()
+
+	originalHTTPClient := http.Client{
+		Transport: r,
+	}
+
+	qiitaClient := qiita.NewClient(&originalHTTPClient)
 	user := qiitaClient.FetchUser("yyh-gl")
 	fmt.Println("============ RESULT ============")
 	fmt.Printf("%+v\n", user)
